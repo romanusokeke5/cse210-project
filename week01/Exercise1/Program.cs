9aac6f59f06647a6749a7f78932e1e7238c781f4
romanusokeke5 <romanusokeke501@gmail.com>
2025-03-17 21:37:59 +0100

    Displayed first name and last name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.writeLine($"your is {last},{First} {last}");
     }
 }
