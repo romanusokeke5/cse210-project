d640bf7d9b064b323b433f4f5a6720645417eb16
romanusokeke5 <romanusokeke501@gmail.com>
2025-03-18 13:49:55 +0100

    Built a guess number game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int numGuesses = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                numGuesses++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {numGuesses} guesses.");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine();
+            playAgain = response.ToLower() == "yes";
+        }
     }
 }
