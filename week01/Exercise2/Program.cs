8d64f4d0a418682ecce435176230c73cf3bcd101
romanusokeke5 <romanusokeke501@gmail.com>
2025-03-18 06:35:45 +0100

    BUit a grade calculator

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int grade = Convert.ToInt32(input);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You got a C!");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You got a D!");
+        }
+        else
+        {
+            Console.WriteLine("You got an F!");
+        }
+
+        string passMessage;
+        if (grade >= 60)
+        {
+            passMessage = "You passed!";
+        }
+        else
+        {
+            passMessage = "You failed!";
+        }
+
+         Console.WriteLine($"Your grade is {grade}. {passMessage}");
+
+
     }
 }
