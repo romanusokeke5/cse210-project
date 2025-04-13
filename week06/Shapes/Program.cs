42912d0923c82b08dcb761095b8b188f8293767f
romanusokeke5 <romanusokeke501@gmail.com>
2025-04-13 21:05:17 +0100

    Polymorphism project

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,37 @@
+// Program.cs
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Verify that the program runs (add a simple line of code to test)
+        Console.WriteLine("Shapes project started!");
+
+        // Test Square class
+        Square testSquare = new Square("Yellow", 5);
+        Console.WriteLine($"Square - Color: {testSquare.GetColor()}, Area: {testSquare.GetArea()}");
+
+        // Test Rectangle class
+        Rectangle testRectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Rectangle - Color: {testRectangle.GetColor()}, Area: {testRectangle.GetArea()}");
+
+        // Test Circle class
+        Circle testCircle = new Circle("Green", 3);
+        Console.WriteLine($"Circle - Color: {testCircle.GetColor()}, Area: {testCircle.GetArea()}");
+
+        // Build a List of Shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(testSquare);
+        shapes.Add(testRectangle);
+        shapes.Add(testCircle);
+
+        // Iterate and display color and area
+        Console.WriteLine("\nShapes in the list:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
